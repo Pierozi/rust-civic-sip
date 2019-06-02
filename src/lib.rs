42f86a6862e029129a95c940888fc588537becd0
Pierre Tomasina <tomasinap@gmail.com>
2019-06-02 16:44:04 +0200

    feat(civic) request exchange

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,17 @@ extern crate frank_jwt as jwt;
 extern crate openssl;
 extern crate chrono;
 extern crate uuid;
+extern crate reqwest;
+
+mod error;
 
 use uuid::Uuid;
 use chrono::Utc;
 use serde::{Serialize, Deserialize};
 use serde_json::json;
+use reqwest::header::{CONTENT_TYPE, CONTENT_LENGTH, ACCEPT, AUTHORIZATION};
+use reqwest::StatusCode;
+use error::CivicError;
 
 use openssl::{
     pkey::Private,
@@ -16,17 +22,23 @@ use openssl::{
 
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
+
 // Create alias for HMAC-SHA256
 type HmacSha256 = Hmac<Sha256>;
 
-const CIVIC_SIP_API_URL: &'static str = "https://api.civic.com/sip/";
+const CIVIC_SIP_API_URL: &'static str = "https://api.civic.com/sip";
 const CIVIC_SIP_API_PUB: &'static str = "049a45998638cfb3c4b211d72030d9ae8329a242db63bfb0076a54e7647370a8ac5708b57af6065805d5a6be72332620932dbb35e8d318fce18e7c980a0eb26aa1";
 const CIVIC_JWT_EXPIRATION: i64 = 180; // 3 min
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Claims {
     sub: String,
     company: String,
+}
+
+#[derive(Deserialize)]
+struct Payload {
+    data: String,
 }
 
 /// CIVIC Application configuration
@@ -34,6 +46,8 @@ pub struct CivicSipConfig {
     pub app_id: &'static str,
     pub app_secret: &'static str,
     pub private_key: &'static str,
+    pub env: &'static str,
+    pub proxy: Option<&'static str>,
 }
 
 pub struct CivicSip {
@@ -50,13 +64,59 @@ impl CivicSip {
     /// # Arguments
     /// * `jwt_token` - A string containing the authorization code (AC)
     ///
-    pub fn exchange_code(&self, jwt_token: &str) -> String {
-        let body = json!({
+    pub fn exchange_code(&self, jwt_token: &str) -> Result<&'static str, CivicError> {
+        let body: String = json!({
             "authToken": jwt_token,
             "processPayload": true,
-        });
-        let authorization: String = self.make_authorization_header(body);
-        return authorization;
+        }).to_string();
+        let auth_header: String = self.make_authorization_header(&body);
+
+        let client = match self.config.proxy {
+            Some(proxy) => reqwest::Client::builder()
+                .proxy(reqwest::Proxy::all(proxy)?)
+                .build()?,
+            _ => reqwest::Client::new(),
+        };
+
+        let mut response = client.post(
+            format!("{}/{}/scopeRequest/authCode", CIVIC_SIP_API_URL, &self.config.env).as_str()
+            )
+            .header(AUTHORIZATION, auth_header)
+            .header(CONTENT_LENGTH, body.len())
+            .header(CONTENT_TYPE, "application/json")
+            .header(ACCEPT, "Accept")
+            .body(body)
+            .send()?;
+
+        return match response.status() {
+            StatusCode::OK => {
+                return self.process_payload(response.json()?);
+            },
+            StatusCode::BAD_REQUEST => Err(CivicError {
+                code: 100400,
+                message: String::from("Exchange code failed!"),
+            }),
+            StatusCode::UNAUTHORIZED => Err(CivicError {
+                code: 100401,
+                message: String::from("Exchange code failed!"),
+            }),
+            StatusCode::FORBIDDEN => Err(CivicError {
+                code: 100403,
+                message: String::from("Exchange code failed!"),
+            }),
+            StatusCode::NOT_FOUND => Err(CivicError {
+                code: 100404,
+                message: String::from("Exchange code failed!"),
+            }),
+            StatusCode::INTERNAL_SERVER_ERROR => Err(CivicError {
+                code: 100500,
+                message: String::from("Exchange code failed!"),
+            }),
+            status => Err(CivicError {
+                code: 1,
+                message: format!("Backend status code not supported: {:?}", status),
+            }),
+        };
     }
 
     /// Convert ECDSA private key from HEX to PEM
@@ -104,7 +164,7 @@ impl CivicSip {
     ///
     /// The token is signed by the application private_key and secret.
     ///
-    fn make_authorization_header(&self, body: serde_json::Value) -> String {
+    fn make_authorization_header(&self, body: &String) -> String {
         let payload = json!({
             "jti": Uuid::new_v4(),
             "iat": Utc::now().timestamp_millis() / 1000,
@@ -137,14 +197,17 @@ impl CivicSip {
 
         // Create CIVIC extension in base64 using hmac with the application secret on the JWT AC
         let mut mac = HmacSha256::new_varkey(self.config.app_secret.as_bytes()).unwrap();
-        mac.input(body.to_string().as_bytes());
+        mac.input(body.as_bytes());
 
         return format!("Civic {}.{}",
                        jwt_token,
                        base64::encode(&mac.result().code().to_owned()));
     }
 
-    fn process_payload(_response: &str) -> &'static str {
-        panic!("Have to develop that!")
+    fn process_payload(&self, payload: Payload) -> Result<&'static str, CivicError> {
+        return Err(CivicError {
+            code: 1,
+            message: format!("DEBUG: {:?}", payload.data),
+        })
     }
 }
