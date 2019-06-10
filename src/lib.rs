850e4a8259f210e524f1a0b1de1effd6559f1726
Pierre Tomasina <tomasinap@gmail.com>
2019-06-10 16:36:15 +0200

    feat(CI) Static Code Analysis

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,27 @@
+extern crate chrono;
 extern crate frank_jwt as jwt;
 extern crate openssl;
-extern crate chrono;
-extern crate uuid;
 extern crate reqwest;
+extern crate uuid;
 
-pub mod error;
 pub mod crypto;
+pub mod error;
 
-use uuid::Uuid;
 use chrono::Utc;
-use serde::{Deserialize};
-use serde_json::{json, Value as JsonValue};
-use reqwest::header::{CONTENT_TYPE, CONTENT_LENGTH, ACCEPT, AUTHORIZATION};
-use reqwest::StatusCode;
 use error::CivicError;
-use sha2::Sha256;
 use hmac::{Hmac, Mac};
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_LENGTH, CONTENT_TYPE};
+use reqwest::StatusCode;
+use serde::Deserialize;
+use serde_json::{json, Value as JsonValue};
+use sha2::Sha256;
+use uuid::Uuid;
 
 // Create alias for HMAC-SHA256
 type HmacSha256 = Hmac<Sha256>;
 
-const CIVIC_SIP_API_URL: &'static str = "https://api.civic.com/sip";
-const CIVIC_SIP_API_PUB: &'static str = "049a45998638cfb3c4b211d72030d9ae8329a242db63bfb0076a54e7647370a8ac5708b57af6065805d5a6be72332620932dbb35e8d318fce18e7c980a0eb26aa1";
+const CIVIC_SIP_API_URL: &str = "https://api.civic.com/sip";
+const CIVIC_SIP_API_PUB: &str = "049a45998638cfb3c4b211d72030d9ae8329a242db63bfb0076a54e7647370a8ac5708b57af6065805d5a6be72332620932dbb35e8d318fce18e7c980a0eb26aa1";
 const CIVIC_JWT_EXPIRATION: i64 = 180; // 3 min
 
 #[derive(Deserialize)]
@@ -54,12 +54,9 @@ impl CivicSip {
             Some(civic_env_enum) => match civic_env_enum {
                 CivicEnv::Prod => "prod",
                 CivicEnv::Dev => "dev",
-            }
+            },
         };
-        CivicSip {
-            config,
-            env,
-        }
+        CivicSip { config, env }
     }
 
     /// Exchange the authorization code wrapped in a JWT token for the requested user data
@@ -71,7 +68,8 @@ impl CivicSip {
         let body: String = json!({
             "authToken": jwt_token,
             "processPayload": true,
-        }).to_string();
+        })
+        .to_string();
         let auth_header: String = self.make_authorization_header(&body);
 
         let client = match self.config.proxy {
@@ -81,9 +79,8 @@ impl CivicSip {
             _ => reqwest::Client::new(),
         };
 
-        let mut response = client.post(
-            format!("{}/{}/scopeRequest/authCode", CIVIC_SIP_API_URL, &self.env).as_str()
-            )
+        let mut response = client
+            .post(format!("{}/{}/scopeRequest/authCode", CIVIC_SIP_API_URL, &self.env).as_str())
             .header(AUTHORIZATION, auth_header)
             .header(CONTENT_LENGTH, body.len())
             .header(CONTENT_TYPE, "application/json")
@@ -91,35 +88,35 @@ impl CivicSip {
             .body(body)
             .send()?;
 
-        return match response.status() {
-            StatusCode::OK => {
-                return self.process_payload(response.json()?);
-            },
+        match response.status() {
+            StatusCode::OK => self.process_payload(response.json()?),
+
             StatusCode::BAD_REQUEST => Err(CivicError {
-                code: 100400,
+                code: 100_400,
                 message: String::from("Exchange code failed!"),
             }),
             StatusCode::UNAUTHORIZED => Err(CivicError {
-                code: 100401,
+                code: 100_401,
                 message: String::from("Exchange code failed!"),
             }),
             StatusCode::FORBIDDEN => Err(CivicError {
-                code: 100403,
+                code: 100_403,
                 message: String::from("Exchange code failed!"),
             }),
             StatusCode::NOT_FOUND => Err(CivicError {
-                code: 100404,
+                code: 100_404,
                 message: String::from("Exchange code failed!"),
             }),
             StatusCode::INTERNAL_SERVER_ERROR => Err(CivicError {
-                code: 100500,
+                code: 100_500,
                 message: String::from("Exchange code failed!"),
             }),
+
             status => Err(CivicError {
                 code: 1,
                 message: format!("Backend status code not supported: {:?}", status),
             }),
-        };
+        }
     }
 
     /// Create the value of Authorization header for the call of CIVIC API
@@ -129,7 +126,7 @@ impl CivicSip {
     ///
     /// The token is signed by the application private_key and secret.
     ///
-    fn make_authorization_header(&self, body: &String) -> String {
+    fn make_authorization_header(&self, body: &str) -> String {
         let payload = json!({
             "jti": Uuid::new_v4(),
             "iat": Utc::now().timestamp_millis() / 1000,
@@ -148,35 +145,34 @@ impl CivicSip {
         });
 
         let private_key: Vec<u8> = crypto::get_private_key_pem(&self.config.private_key);
-        let jwt_token = match frank_jwt::encode(
-            header,
-            &private_key,
-            &payload,
-            frank_jwt::Algorithm::ES256
-        ) {
-            Ok(token) => token,
-            Err(error) => {
-                panic!("There was a problem during JWT ENCODE: {:?}", error);
-            },
-        };
+        let jwt_token =
+            match frank_jwt::encode(header, &private_key, &payload, frank_jwt::Algorithm::ES256) {
+                Ok(token) => token,
+                Err(error) => {
+                    panic!("There was a problem during JWT ENCODE: {:?}", error);
+                }
+            };
 
         // Create CIVIC extension in base64 using hmac with the application secret on the JWT AC
         let mut mac = HmacSha256::new_varkey(self.config.app_secret.as_bytes()).unwrap();
         mac.input(body.as_bytes());
 
-        return format!("Civic {}.{}",
-                       jwt_token,
-                       base64::encode(&mac.result().code().to_owned()));
+        return format!(
+            "Civic {}.{}",
+            jwt_token,
+            base64::encode(&mac.result().code().to_owned())
+        );
     }
 
     /// Process CIVIC response
     /// decrypt data using app secret and return result as JsonValue
     fn process_payload(&self, payload: Payload) -> Result<JsonValue, CivicError> {
-        return match crypto::decode(&payload.data, CIVIC_SIP_API_PUB) {
+        match crypto::decode(&payload.data, CIVIC_SIP_API_PUB) {
             Err(error) => Err(error),
-            Ok((_, jwt_payload)) => {
-                return crypto::decrypt(jwt_payload["data"].as_str().unwrap(), &self.config.app_secret);
-            }
-        };
+            Ok((_, jwt_payload)) => crypto::decrypt(
+                jwt_payload["data"].as_str().unwrap(),
+                &self.config.app_secret,
+            ),
+        }
     }
 }
